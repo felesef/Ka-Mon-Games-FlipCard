8ab858b3263221ccbe418c5319834b5ac61cd9a8
MonikaShaik <monika.shaik@outlook.com>
2026-02-17 21:03:50 +0100

    Add database setup file

---
+++ db-setup.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE `MemoryGameDB`;
+
+-- MemoryGameDB.Cards definition
+
+CREATE TABLE `MemoryGameDB`.`Cards` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `name` varchar(100) NOT NULL,
+    `imgURL` varchar(2048) NOT NULL,
+    `theme` varchar(100) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 61 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+
+-- MemoryGameDB.Scores definition
+
+CREATE TABLE `MemoryGameDB`.`Scores` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `scores` int NOT NULL,
+    `playerName` varchar(100) NOT NULL,
+    `date` datetime NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
